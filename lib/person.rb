7386a0002b994215cbc49e1ad40549c6c8da491f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 05:18:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
     :weight, :handed, :complexion, :t_shirt_size, 
     :wrist_size, :glove_size, :pant_length, :pant_width
     
-   def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
+    def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
         :weight, :handed, :complexion, :t_shirt_size, 
         :wrist_size, :glove_size, :pant_length, :pant_width)
     @name = name
@@ -20,5 +20,5 @@ class Person
     @glove_size = glove_size
     @pant_length = pant_length
     @pant_width = pant_width
-  end
+   end
 end
