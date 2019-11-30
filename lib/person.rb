3047edc8bbe2591753452630f5b599a8448b4ba2
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 05:53:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 require "pry"
 class Person
   #your code here
-    attr_accessor:name,:birthday,:hair_color
+    attr_accessor:name,:birthday,:hair_color,:eye_color
     
     def initialize(attributes)
         #binding.pry
