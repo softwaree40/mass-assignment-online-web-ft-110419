27507c1518754a3ab2346359bc7f4eda0760281c
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 05:24:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
     attr_accessor:name,:birthday,:hair_color,:eye_color,:height, 
     :weight,:handed,:complexion,:t_shirt_size,:wrist_size,:glove_size,:pant_length,:pant_width
     
-    def initialize(:name,:birthday,:hair_color,:eye_color,:height,:weight,:handed,:complexion,:t_shirt_size, 
+    def initialize(name:,birthday:,hair_color:,:eye_color,:height,:weight,:handed,:complexion,:t_shirt_size, 
         :wrist_size,:glove_size,:pant_length,:pant_width)
        @name = name
        @birthday = birthday
