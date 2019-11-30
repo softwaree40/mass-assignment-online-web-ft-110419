27830e38a1a65dfadc0b0cc8c095161bc0accef2
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 05:51:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
     
     def initialize(attributes)
         #binding.pry
-        @name = name
+        attributes.each {|key, value| self.send(("#{key}="), value)}
        
    end
    
