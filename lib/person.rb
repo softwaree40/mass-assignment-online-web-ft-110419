3681116c32b1d16a9d7d4fb6883806ee1cc3aa2f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 05:43:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   #your code here
     attr_accessor:birthday,:hair_color,:eye_color,:height,:weight,:handed,:complexion,:t_shirt_size,:wrist_size,:glove_size,:pant_length,:pant_width
     
-    def initialize(birthday:,eye_color:,complexion:,t_shirt_size:,wrist_size:,glove_size:,pant_length:,pant_width:)
+    def initialize(birthday:,hair_color:,eye_color:,height:,weight:,handed:,complexion:,t_shirt_size:,wrist_size:,glove_size:,pant_length:,pant_width:)
        @birthday = birthday
        @eye_color = eye_color
        @complexion = complexion
